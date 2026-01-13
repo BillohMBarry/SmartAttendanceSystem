a2c7fbcdfa2e2694638e005277a7dbda4b311f84
BillohMBarry <alhajibillohbarrie@gmail.com>
2026-01-13 03:34:04 -0800

    import jwt-secret

--- Backend/src/utils/token.ts
+++ Backend/src/utils/token.ts
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from '../config/config.ts';
+import { JWT_SECRET } from '../config/config.js';
 
 interface QRTokenPayload {
     officeId: string;
