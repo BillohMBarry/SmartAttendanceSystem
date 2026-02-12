319ff9c62b8a1f11de097e44c2e2c991b44b867d
BillohMBarry <alhajibillohbarrie@gmail.com>
2026-02-12 14:27:29 +0000

    assign existing employee to office address

---
+++ Backend/src/scripts/assignOfficeToEmployees.ts
@@ -0,0 +1,20 @@
+import { User } from '../models/Users.js';
+import mongoose from 'mongoose';
+import { mongodbUri } from '../config/config.js';
+
+(async () => {
+    await mongoose.connect(mongodbUri);
+
+   const admin = await User.findOne({ role: 'admin' });
+   if(!admin || !admin.office) {
+    console.error('Admin or admin office not found');
+    process.exit(1);
+   }
+
+  await User.updateMany(
+    { role: 'employee', office: { $exists: false } },
+    { $set: { office: admin.office } },
+  )
+  console.log('Office assigned to employees successfully');
+  process.exit(0);
+})()
