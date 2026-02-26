78af8ec475c60b7f11cb9d117aca0d8ea2bc9dcd
CMDA-SL <cmdasldev26@gmail.com>
2026-02-26 16:17:14 +0000

    update existing employee to match the office cordinate

--- Backend/src/scripts/assignOfficeToEmployees.ts
+++ Backend/src/scripts/assignOfficeToEmployees.ts
@@ -3,16 +3,22 @@ import mongoose from 'mongoose';
 import { mongodbUri } from '../config/config.js';
 
 (async () => {
-    await mongoose.connect(mongodbUri);
+  await mongoose.connect(mongodbUri);
 
-   const admin = await User.findOne({ role: 'admin' });
-   if(!admin || !admin.office) {
+  const admin = await User.findOne({ role: 'admin' });
+  if (!admin || !admin.office) {
     console.error('Admin or admin office not found');
     process.exit(1);
-   }
+  }
 
   await User.updateMany(
-    { role: 'employee', office: { $exists: false } },
+    {
+      role: 'employee',
+      $or: [
+        { office: { $exists: false } },
+        { office: null }
+      ]
+    },
     { $set: { office: admin.office } },
   )
   console.log('Office assigned to employees successfully');
